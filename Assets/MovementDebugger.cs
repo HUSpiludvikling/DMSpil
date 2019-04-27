5d7a54fd505386546316aab83f23e9bd43b9281a
Unknown <cmolln10@student.aau.dk>
2019-04-27 14:57:38 +0200

    Masser af input

---
+++ Assets/MovementDebugger.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementDebugger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("JumpJ1"))
+        {
+            Debug.Log("1F");
+        }
+        if (Input.GetButtonDown("JumpJ2"))
+        {
+            Debug.Log("2F");
+        }
+        if (Input.GetButtonDown("JumpJ3"))
+        {
+            Debug.Log("3F");
+        }
+
+        if(Input.GetAxis("HorizontalJ1") != 0)
+        {
+            Debug.Log("H1" + Input.GetAxis("HorizontalJ1"));
+        }
+
+        if (Input.GetAxis("HorizontalJ2") != 0)
+        {
+            Debug.Log("H2");
+        }
+
+        if (Input.GetAxis("HorizontalJ3") != 0)
+        {
+            Debug.Log("H3");
+        }
+
+
+    }
+}
