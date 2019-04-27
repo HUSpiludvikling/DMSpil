f0c8befd63ad5dd1da3df37b033b732d14a0cfb4
Unknown <cmolln10@student.aau.dk>
2019-04-27 16:45:43 +0200

    Lyd, baggrund.

---
+++ Assets/BGScroller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BGScroller : MonoBehaviour
+{
+    Image img;
+    // Start is called before the first frame update
+    void Start()
+    {
+        img = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        img.material.SetTextureOffset("_MainTex", new Vector2((Time.timeSinceLevelLoad / 4f) % img.mainTexture.width, 0f));
+        //img.material.SetTextureOffset("_MainTex", img.material.GetTextureOffset("_MainTex") + Vector2.right);
+    }
+}
---
+++ Assets/PlaySoundOn.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public enum SoundOnTypes
+{
+    Jump, 
+    Spec
+}
+
+[RequireComponent(typeof(AudioSource))]
+public class PlaySoundOn : MonoBehaviour
+{
+    AudioSource _as;
+
+    [SerializeField] SoundOnTypes SoundOn;
+
+    UnityEvent soundTrigger;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _as = GetComponent<AudioSource>();
+
+        switch (SoundOn)
+        {
+            case SoundOnTypes.Jump:
+                GetComponentInParent<ActorMovementManager>().jumpEvent.AddListener(PlaySound); break;
+            case SoundOnTypes.Spec:
+                GetComponentInParent<ActorMovementManager>().specEvent.AddListener(PlaySound); break;
+            default:
+                break;
+        }
+
+    }
+
+    void PlaySound()
+    {
+        if(!_as.isPlaying)
+        {
+            _as.Play();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
