dedcc446771a8a7db71ca2087b4192322a5a137c
Unknown <cmolln10@student.aau.dk>
2019-04-28 10:07:27 +0200

    Door

---
+++ Assets/DoorToNextLevel.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorToNextLevel : MonoBehaviour
+{
+    List<ActorMovementManager> AMMs;
+
+    [SerializeField] string load;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AMMs = new List<ActorMovementManager>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        ActorMovementManager amm = collision.GetComponent<ActorMovementManager>();
+        AMMs.Add(amm);
+
+        amm.specEvent.AddListener(LoadLevel);
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        ActorMovementManager amm = collision.GetComponent<ActorMovementManager>();
+        if(amm != null && AMMs.Contains(amm))
+        {
+
+            amm.specEvent.RemoveListener(LoadLevel);
+            AMMs.Remove(amm);
+        }
+    }
+
+    void LoadLevel()
+    {
+        foreach (var item in AMMs)
+        {
+            item.specEvent.RemoveListener(LoadLevel);
+        }
+
+        SceneManager.LoadScene(load);
+    }
+
+}
