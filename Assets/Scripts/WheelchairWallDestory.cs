3eacb0d59d13548c0f5d2fb767607855035e9edb
Unknown <cmolln10@student.aau.dk>
2019-04-28 10:13:35 +0200

    Opdatering til wheelchair destroyt

--- Assets/Scripts/WheelchairWallDestory.cs
+++ Assets/Scripts/WheelchairWallDestory.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class WheelchairWallDestory : MonoBehaviour
@@ -29,10 +30,28 @@ public class WheelchairWallDestory : MonoBehaviour
                 Debug.Log("CollisionEnter1");
 
                 gameObject.GetComponent<BoxCollider2D>().enabled = false;
+
+                StartCoroutine(rollAway());
                 //anim.SetBool("DestoryWall", true);
 
             }
 
         }
     }
+
+    IEnumerator rollAway()
+    {
+        List<Renderer> rends = GetComponentsInChildren<Renderer>().ToList();
+        while (rends.Count != 0)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                transform.position += Vector3.down / 10.0f;
+                yield return new WaitForFixedUpdate();
+            }
+            rends.First().enabled = false;
+            rends.Remove(rends.First());
+        }
+    }
+
 }
